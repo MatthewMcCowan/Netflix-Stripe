4785bfcbc6dd3aa0d99f68f45338d1d9997a8a06
matthew <matthewmccowan@yahoo.com>
2021-02-05 00:12:00 -0600

    Firebase Setup and File Cleanup

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0hEbOUPffo17Yn1jw0HAHfTrwUJASeCc",
+  authDomain: "netflix-stripe.firebaseapp.com",
+  projectId: "netflix-stripe",
+  storageBucket: "netflix-stripe.appspot.com",
+  messagingSenderId: "348479990744",
+  appId: "1:348479990744:web:2a317bcc52fc658f1160ad",
+};
